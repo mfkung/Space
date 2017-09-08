0e5b2440579648a710f75cdddde5f093fc397705
Peeranat Na nakorn <mfkung_2@hotmail.com>
2017-09-08 15:19:00 +0700

    Add hit gold and then random location

--- models.py
+++ models.py
@@ -1,10 +1,13 @@
 import arcade.key
+from random import randint
 class Model:
     def __init__(self, world, x, y, angle):
         self.world = world
         self.x = x
         self.y = y
         self.angle = 0
+    def hit(self, other, hit_size):
+        return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
 
 class Ship(Model):
     DIR_HORIZONTAL = 0
@@ -45,7 +48,13 @@ class World:
             self.ship.switch_direction()
     def update(self, delta):
         self.ship.update(delta)
+ 
+        if self.ship.hit(self.gold, 15):
+            self.gold.random_location()
 
 class Gold(Model):
     def __init__(self, world, x, y):
         super().__init__(world, x, y, 0)
+    def random_location(self):
+        self.x = randint(0, self.world.width - 1)
+        self.y = randint(0, self.world.height - 1)
